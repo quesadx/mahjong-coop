c85446df396dd14afd24969c3555d5dab2f17f93
Matteo Quesada <matteo.vargas.quesada@est.una.ac.cr>
2026-04-06 18:48:02 -0600

    Add nix-direnv-reload script and initial .envrc and flake.nix files

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Web template: Node + pnpm + TypeScript";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nodejs_20
+        ];
+      };
+    };
+}
